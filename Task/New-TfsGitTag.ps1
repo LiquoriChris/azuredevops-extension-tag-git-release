b96e7bdaf12e463913621e1357bf73170a6d68d9
Chris Liquori <chrisl@epi.usf.edu>
2019-03-08 15:28:50 -0500

    Fix personal access token variable.

--- Task/New-TfsGitTag.ps1
+++ Task/New-TfsGitTag.ps1
@@ -35,6 +35,7 @@ if (!$PersonalAccessToken -and !$OAuthToken) {
 else {
     $Params = @{}
     if ($PersonalAccessToken) {
+        $PersonalAccessToken = Get-VstsInput -Name 'PersonalAccessToken' -Default false
         $Params.PersonalAccessToken = $PersonalAccessToken
     }
     $Headers = _Authentication @Params
